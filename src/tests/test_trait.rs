08d3de247edd47581b97a0b61a1808238ddbb0ef
Garrett Berg <googberg@gmail.com>
2016-05-27 00:22:38 -0600

    trait tests were broken somehow

--- src/tests/test_trait.rs
+++ src/tests/test_trait.rs
@@ -7,6 +7,6 @@ fn test_trait() {
     let mut vars: HashMap<String, String> = HashMap::new();
     vars.insert("x".to_string(), "X".to_string());
 
-    assert_eq!("hi {x}".format(&vars), "hi X");
-    assert_eq!("hi {x}".to_string().format(&vars), "hi X");
+    assert_eq!("hi {x}".format(&vars).unwrap(), "hi X");
+    assert_eq!("hi {x}".to_string().format(&vars).unwrap(), "hi X");
 }

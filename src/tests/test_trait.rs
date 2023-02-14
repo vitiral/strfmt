7153b83775cce80e2ecb93393810ea352621b6b8
Luke Peterson <luke@infoplatter.com>
2023-02-14 09:59:08 +0900

    Adding test_heterogenous_values, to exercise DisplayStr implementation for &dyn DisplayStr

--- src/tests/test_trait.rs
+++ src/tests/test_trait.rs
@@ -9,3 +9,14 @@ fn test_trait() {
     assert_eq!("hi {x}".format(&vars).unwrap(), "hi X");
     assert_eq!("hi {x}".to_string().format(&vars).unwrap(), "hi X");
 }
+
+#[test]
+fn test_heterogenous_values() {
+    let mut data: HashMap<String, &dyn DisplayStr> = HashMap::new();
+    data.insert("body".to_string(), &"someString");
+    data.insert("some_number".to_string(), &5.0);
+
+    let output = strfmt("{body} = {some_number:2.4}", &data).unwrap();
+
+    assert_eq!(output, "someString = 5.0000");
+}
